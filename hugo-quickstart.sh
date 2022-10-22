0d2c83f26c396aa5b4e971e7e027639d6c088347
root <root@ip-172-31-0-176.us-west-1.compute.internal>
2022-10-22 17:58:08 +0000

    first commit

--- hugo-quickstart.sh
+++ hugo-quickstart.sh
@@ -3,4 +3,4 @@
 #***
 sudo rm -dr ~/hugo-site/
 #***
-mkdir ~/hugo-site/ && cd ~/hugo-site && hugo new site quickstart && cd ~/hugo-site/quickstart/themes && git clone https://github.com/jesselau76/hugo-w3-simple.git && cp -r ~/hugo-site/quickstart/themes/hugo-w3-simple/exampleSite/ ~/hugo-site/quickstart/ && cd ~/hugo-site/quickstart/ && hugo serve
+mkdir ~/hugo-site/ && cd ~/hugo-site && hugo new site quickstart && cd ~/hugo-site/quickstart/themes && git clone https://github.com/jesselau76/hugo-w3-simple.git && cp -r ~/hugo-site/quickstart/themes/hugo-w3-simple/exampleSite/config.toml ~/hugo-site/quickstart/ && cd ~/hugo-site/quickstart/ && hugo serve --bind "0.0.0.0"
